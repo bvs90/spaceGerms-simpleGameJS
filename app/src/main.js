de5c82089525915283e28faba9b22fa9243d4817
Ben Stone <benjamin.v.stone@gmail.com>
2015-05-17 15:46:04 -0700

    Add scrolling background

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,17 @@
 window.addEventListener('load', init);
 
+var game;
+
 function init() {
+  game = new Scene();
   
+  background = new Background();
+  
+  game.start();
 }
 
 function update() {
+  game.clear();
   
+  background.update();
 }
---
+++ app/src/sprites.js
@@ -0,0 +1,14 @@
+function Background() {
+  var background = new Sprite(game, './assets/space-background.gif', 800, 1200);
+  background.setDX(0);
+  background.setDY(0.5);
+  background.setPosition(400,300);
+  
+  background.checkBounds = function(){
+    if (this.y > 600){
+      this.setPosition(400, 0);
+    }      
+  };
+  
+  return background;
+}
