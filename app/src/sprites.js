17d25e7f3435138d84b446b9e1ee7f9f8042741a
Ben Stone <benjamin.v.stone@gmail.com>
2015-05-18 19:16:06 -0700

    Add forward and backward movement to player sprite

--- app/src/sprites.js
+++ app/src/sprites.js
@@ -15,7 +15,7 @@ function Background() {
 
 function SpaceShip() {
   var spaceShip = new Sprite(game, './assets/test-ship.png', 41, 78);
-  spaceShip.setSpeed(0);
+  spaceShip.setSpeed(1);
   spaceShip.setPosition(400, 550);
   
   spaceShip.checkKeys = function() {
@@ -24,9 +24,14 @@ function SpaceShip() {
     }
     if (keysDown[K_RIGHT]) {
       this.changeXby(10);
-    }  
+    }
+    if (keysDown[K_UP]) {
+      this.changeYby(-10);
+    }
+    if (keysDown[K_DOWN]) {
+      this.changeYby(10);
+    }   
   };
-  
   
   return spaceShip;
 }
